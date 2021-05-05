b1e79d11da55663919b91f7df0342a56e5716012
ElevateBart <ledouxb@gmail.com>
2021-05-05 09:57:04 -0500

    fix: remove unnecessary patch package

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,7 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+require("@cypress/instrument-cra");
 const injectCraDevServer = require("@cypress/react/plugins/react-scripts");
 const installCoverageTask = require("@cypress/code-coverage/task");
 
